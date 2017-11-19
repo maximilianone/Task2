5e4207cd1740e0f5052e617109f54827896b8c0d
maximilianone <maximilianone22@gmail.com>
2017-11-19 14:24:22 +0200

    Add files via upload

---
+++ src/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+
+import test.Views.MenuView;
+import test.controller.Menu;
+
+import static test.LibraryCreation.createLibrary;
+
+public class Test {
+    public static void main(String[] args) {
+        Book[] libraryTest = createLibrary();
+        MyScanner scanner = new MyScanner();
+        MenuView menuView = new MenuView();
+        Menu menu = new Menu(libraryTest, menuView, scanner);
+        menu.run();
+        scanner.closeScanner();
+    }
+}
